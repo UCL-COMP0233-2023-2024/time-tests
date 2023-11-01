891efaf0af9ac57877a5ef14366faa96e6cabed5
superheroa6 <zczqjy2@ucl.ac.uk>
2023-11-01 22:07:20 +0000

    commit times.py

--- times.py
+++ times.py
@@ -18,12 +18,38 @@ def compute_overlap_time(range1, range2):
             low = max(start1, start2)
             high = min(end1, end2)
             overlap_time.append((low, high))
-    return overlap_time
+    # print("overlap_time = " + overlap_time)
+    if overlap_time[0][0] > overlap_time[0][1]:
+        raise ValueError("There is no overlap.")
+    elif overlap_time[0][0] == overlap_time[0][1]:
+        return str(overlap_time[0][0]) # not a list-how to transform
+    else:
+        return overlap_time
+    # return overlap_time
 
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
 
-    print(large)
-    print(short)
+
+    # test for no overlap ranges
+    range0 = time_range("2010-01-12 09:00:00", "2010-01-12 09:59:59")
+    # print(compute_overlap_time(range0, large))
+ 
+    # test for a point overlap
+    range1 = time_range("2010-01-12 09:00:00", "2010-01-12 10:00:00")
+    print(compute_overlap_time(range1, large)) # 不是一个list
+    
+    # test for partial overlap
+    range2 = time_range("2010-01-12 09:30:00", "2010-01-12 10:40:00")
+    print(compute_overlap_time(short, range2))
+    # if range2 has some intervals, the test would fail
+
+
+
+    # test for full overlap
+    # print(compute_overlap_time(large, short))
+
+
+
