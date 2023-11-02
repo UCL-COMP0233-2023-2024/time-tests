fb6ee25e618814346f1a9f326f81ebf773ff9878
superheroa6 <zczqjy2@ucl.ac.uk>
2023-11-02 11:13:45 +0000

    11

--- test_times.py
+++ test_times.py
@@ -1,5 +1,5 @@
 from times import time_range, compute_overlap_time
-
+import pytest
 
 def test_full_overlap():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -12,17 +12,15 @@ def test_full_overlap():
 def test_no_overlap():
     range0 = time_range("2010-01-12 09:00:00", "2010-01-12 09:59:59", 2, 60)
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    result = compute_overlap_time(large, range0)
-    expected = ValueError("There is no overlap")
-    assert result == expected
-
+    with pytest.raises(ValueError):
+        compute_overlap_time(range0, large)
 
 
 def test_point_overlap():
     range1 = time_range("2010-01-12 09:00:00", "2010-01-12 10:00:00") # fail if have intervals????????????????
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     result = compute_overlap_time(large, range1)
-    expected = "2010-01-12 10:00:00" #problem: not a list--how to solve
+    expected = ["2010-01-12 10:00:00"] #problem: not a list--how to solve
     assert result == expected
 
 def test_partial_overlap():
@@ -32,5 +30,16 @@ def test_partial_overlap():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:40:00')]
     assert result == expected
 
+def test_several_intervals():
+    range3 = time_range("2010-01-12 09:30:00", "2010-01-12 10:40:00", 2, 60)  
+    short = time_range("2010-01-12 10:00:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(range3, short)
+    expected = [('2010-01-12 10:00:00', '2010-01-12 10:04:30'), ('2010-01-12 10:23:00', '2010-01-12 10:04:30'), 
+                ('2010-01-12 10:05:30', '2010-01-12 10:22:00'), ('2010-01-12 10:23:00', '2010-01-12 10:40:00')]
+    assert result == expected
     
-
+def test_range():
+    start = '2010-01-12 10:00:00'
+    end = '2010-01-12 09:59:59'
+    with pytest.raises(ValueError):
+        time_range(start, end, 2, 60)
