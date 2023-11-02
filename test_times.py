105fb3929383434ac2fa8a5ef0d71c0028719ae8
Tianyang Xie <994220475@qq.com>
2023-11-02 09:54:05 +0000

    Commit time test

--- test_times.py
+++ test_times.py
@@ -1,4 +1,5 @@
 from times import time_range,compute_overlap_time
+import pytest
 
 
 def test_given_result():
@@ -7,3 +8,15 @@ def test_given_result():
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:32:00'), ('2010-01-12 10:32:36', '2010-01-12 10:34:36'), ('2010-01-12 10:35:12', '2010-01-12 10:37:12'), ('2010-01-12 10:37:48', '2010-01-12 10:39:48'), ('2010-01-12 10:40:24', '2010-01-12 10:42:24'), ('2010-01-12 10:43:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def no_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short =  time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
+    result = compute_overlap_time(large,short)
+    expected = []
+    assert result == expected
+
+large = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+print(large)
+short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
+print(compute_overlap_time(large,short))
