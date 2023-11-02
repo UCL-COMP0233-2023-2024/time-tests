666fcd8f86525fa3aa19ca8adf6df0e50303e347
Nicola Diurno <ndiurno93@gmail.com>
2023-11-02 11:29:10 +0000

    Run a negative test

--- test_time.py
+++ test_time.py
@@ -1,4 +1,5 @@
 import times
+import pytest
 
 def test_given_input():
     range1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -14,11 +15,8 @@ def test_given_input():
     print("Actual Overlap:", overlap)
     print("Expected Overlap:", expected_overlap)
 
-    
     assert overlap == expected_overlap
 
-if __name__ == "__main__":
-    test_given_input()
 
 from times import time_range, compute_overlap_time
 
@@ -36,19 +34,31 @@ def test_non_overlapping_ranges():
 
     assert overlap == expected_overlap
 
-if __name__ == "__main__":
-    test_non_overlapping_ranges()
 
-if __name__ == "__main":
-    # Define two time ranges with multiple intervals
-    range3 = time_range("2023-01-01 08:00:00", "2023-01-01 09:00:00", 3, 30)
-    range4 = time_range("2023-01-01 08:30:00", "2023-01-01 09:30:00", 3, 30)
+def test_several_overlapping_intervals_ranges():
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00",2,30)
+    range2 = time_range("2010-01-12 11:30:00", "2010-01-12 11:45:00",2,30)
+    overlap = compute_overlap_time(range1, range2)
 
-    # Calculate the overlap between the two time ranges
-    overlap = compute_overlap_time(range3, range4)
+    # Define the expected overlap intervals, which should be an empty list
+    expected_overlap = []
 
-    # Check if there is an overlap
-    if overlap:
-        print("There is overlap between the two time ranges.")
-    else:
-        print("The two time ranges do not overlap.")
+    # Print the actual and expected overlap intervals just to check
+    print("Actual Overlap:", overlap)
+    print("Expected Overlap:", expected_overlap)
+
+    assert overlap == expected_overlap
+
+def test_ranges_starting_when_one_end():
+    range1 = time_range("2023-01-01 08:00:00", "2023-01-01 09:00:00")
+    range2 = time_range("2023-01-01 09:00:00", "2023-01-01 10:00:00")
+    overlap = compute_overlap_time(range1, range2)
+
+    expected_overlap = [("2023-01-01 09:00:00", "2023-01-01 09:00:00")]
+
+    assert overlap == expected_overlap
+
+def test_correct_input():
+
+    with pytest.raises(ValueError):
+        time_range("2023-01-01 09:00:00", "2023-01-01 08:00:00") 
