bb3445128135f59b2ed19fe269c6633da387043b
Edoardo Critelli <e.crite@hotmail.it>
2023-11-02 10:08:15 +0000

    odd/even split/gap combination test

--- test_times.py
+++ test_times.py
@@ -21,10 +21,15 @@ def test_time_range_1split_1minute():
 def test_time_range_4splits_5minutes():
     assert(time_range(test_range1[0],test_range1[1],4,300)) == [('2010-01-12 10:00:00', '2010-01-12 10:26:15'), ('2010-01-12 10:31:15', '2010-01-12 10:57:30'), ('2010-01-12 11:02:30', '2010-01-12 11:28:45'), ('2010-01-12 11:33:45', '2010-01-12 12:00:00')]
 
+def test_time_range_evensplit_oddgap():
+    assert(time_range(test_range1[0],test_range1[1],2,1)) == [("2010-01-12 10:00:00", "2010-01-12 10:59:59"),("2010-01-12 11:00:00","2010-01-12 12:00:00")]
+
+
 test_time_range()
 test_time_range_1split()
 test_time_range_1split_1minute()
 test_time_range_4splits_5minutes()
+test_time_range_evensplit_oddgap()
 
 from unittest import TestCase
 from unittest.mock import Mock
@@ -48,8 +53,6 @@ if __name__ == "__main__":
 def compute_empty_overlap_time():
     test_range1 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     test_range2 = time_range("2010-01-13 22:30:00", "2010-01-13 22:45:00")
-    print(test_range1)
-    print(test_range2)
     result = compute_overlap_time(test_range1,test_range2)
     assert(result) == []
 
