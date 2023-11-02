3c0fb91ef32578287a88069b9bad7c097f8768e3
Tianyang Xie <994220475@qq.com>
2023-11-02 09:58:08 +0000

    Add the test no overlap

--- test_times.py
+++ test_times.py
@@ -18,8 +18,7 @@ def second_test():
                 ('2010-01-12 10:35:12', '2010-01-12 10:37:12'), ('2010-01-12 10:37:48', '2010-01-12 10:39:48'), 
                 ('2010-01-12 10:40:24', '2010-01-12 10:42:24'), ('2010-01-12 10:43:00', '2010-01-12 10:45:00')]
     assert result == expected
-
-second_test()    
+  
 
 def no_overlap():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -27,8 +26,3 @@ def no_overlap():
     result = compute_overlap_time(large,short)
     expected = []
     assert result == expected
-
-large = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
-print(large)
-short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
-print(compute_overlap_time(large,short))
