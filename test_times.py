63d2544a68998a84140057bf91f3fd05e27299da
Tianyang Xie <994220475@qq.com>
2023-11-02 10:05:01 +0000

    Done the first two codes

--- test_times.py
+++ test_times.py
@@ -7,7 +7,7 @@ def test_given_result():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 6, 36)
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:32:00'), ('2010-01-12 10:32:36', '2010-01-12 10:34:36'), ('2010-01-12 10:35:12', '2010-01-12 10:37:12'), ('2010-01-12 10:37:48', '2010-01-12 10:39:48'), ('2010-01-12 10:40:24', '2010-01-12 10:42:24'), ('2010-01-12 10:43:00', '2010-01-12 10:45:00')]
-    assert result == expected
+    return result == expected
 
 def second_test():
     # two time ranges that both contain several intervals each
@@ -17,7 +17,7 @@ def second_test():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:32:00'), ('2010-01-12 10:32:36', '2010-01-12 10:34:36'), 
                 ('2010-01-12 10:35:12', '2010-01-12 10:37:12'), ('2010-01-12 10:37:48', '2010-01-12 10:39:48'), 
                 ('2010-01-12 10:40:24', '2010-01-12 10:42:24'), ('2010-01-12 10:43:00', '2010-01-12 10:45:00')]
-    assert result == expected
+    return result == expected
   
 
 def no_overlap():
@@ -25,4 +25,9 @@ def no_overlap():
     short =  time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
     result = compute_overlap_time(large,short)
     expected = []
-    assert result == expected
+    return result == expected
+
+def test_fun():
+    assert test_given_result()
+    assert second_test()
+    assert no_overlap()
