627339c8b2fa995a4179ed380f66cce74e82231f
Sarah <sarahjanelittlejohn@gmail.com>
2023-11-02 10:11:15 +0000

    Added tests and fixed bugs
    UCL-COMP0233-23-24/RSE-Classwork#17

--- test_times.py
+++ test_times.py
@@ -1,8 +1,33 @@
 from times import compute_overlap_time, time_range
 
+# Positive test
 def test_given_input():
-    large = [('2010-01-12 10:00:00', '2010-01-12 12:00:00')]
-    short = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large, short) 
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    assert result == expected
+
+# time ranges don't overlap
+def test_no_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2011-01-12 10:30:00", "2011-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large, short)
+    expected = ['0000-00-00 00:00:00', '0000-00-00 00:00:00']
+    assert result == expected
+
+# time ranges that both contain several intervals
+def test_multiple_intervals():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large, short)
+    expected =     [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00'), ('2010-01-12 11:00:30', '2010-01-12 10:37:00'), ('2010-01-12 11:00:30', '2010-01-12 10:45:00')]
+    assert result == expected
+
+# test for end1 = start2
+def test_time_ranges_touch():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 13:45:00")
+    result = compute_overlap_time(large, short)
+    expected = ['2010-01-12 12:00:00']
     assert result == expected
