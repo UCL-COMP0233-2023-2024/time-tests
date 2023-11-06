475579ecfdc796ab0d64c332fba40df6c791de8b
Joonmin <zcemjh1@ucl.ac.uk>
2023-11-06 16:04:08 +0000

    added 3 new tests

--- test_times.py
+++ test_times.py
@@ -11,6 +11,27 @@ def test_given_input():
         expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
         assert result == expected
 
-test_given_input()
+def test_do_not_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2011-01-12 10:30:00", "2011-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large, short)
+    expected = ['0000-00-00 00:00:00', '0000-00-00 00:00:00']
+    assert result == expected
+
+def test_several_intervals():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large, short)
+    expected =     [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00'), ('2010-01-12 11:00:30', '2010-01-12 10:37:00'), ('2010-01-12 11:00:30', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_time_ranges_touch():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 13:45:00")
+    result = compute_overlap_time(large, short)
+    expected = ['2010-01-12 12:00:00']
+    assert result == expected
+
+
         
 
--- times.py
+++ times.py
@@ -15,9 +15,14 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            low = max(start1, start2)
-            high = min(end1, end2)
-            overlap_time.append((low, high))
+            if end1 < start2:
+                overlap_time.append("0000-00-00 00:00:00")
+            elif end1 == start2:
+                overlap_time.append(end1)
+            else:
+                low = max(start1, start2)
+                high = min(end1, end2)
+                overlap_time.append((low, high))
     return overlap_time
 
 if __name__ == "__main__":
